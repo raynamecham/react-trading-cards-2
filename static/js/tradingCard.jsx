7a95666fe6e9506cf3bb819bd497b0aafc371895
Rayna Mecham <rayna.mecham@gmail.com>
2021-04-17 19:21:13 -0600

    trying to get image to show up

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,63 +1,38 @@
-var tradingCardData = [
-  {
-    name: 'Balloonicorn',
-    skill: 'video games',
-    imgUrl: '/static/img/balloonicorn.jpg'
-  },
-
-  {
-    name: 'Float',
-    skill: 'baking pretzels',
-    imgUrl: '/static/img/float.jpg'
-  },
-
-  {
-    name: 'Llambda',
-    skill: 'knitting scarves',
-    imgUrl: '/static/img/llambda.jpg'
-  }
-];
-
 function TradingCard(props) {
+
+  // let img = new Image();
+  // img.src = '/static/img/placeholder.png';
+
+  // if (!props.imgUrl) {
+  //   props.imgUrl = img.src;
+  // }
+
   return (
     <div className="card">
-      <p>Name: {props.name}</p>
+      <p> Name: {props.name} </p>
       <img src={props.imgUrl} />
-      <p>Skill: {props.skill} </p>
+      <p> Skill: {props.skill} </p>
     </div>
   );
 }
 
-// function TradingCardContainer() {
-//   const tradingCards = [];
-
-//   for (const currentCard of tradingCardData) {
-//     tradingCards.push(
-//       <TradingCard
-//         key={currentCard.name}
-//         name={currentCard.name}
-//         skill={currentCard.skill}
-//         imgUrl={currentCard.imgUrl}
-//       />
-//     );
-//   }
-
-//   return (
-//     <div>{tradingCards}</div>
-//   );
-// }
-
 function TradingCardContainer() {
+  const [cards, updateCards] = React.useState([]);
 
-  const floatCard = {
-    name: 'Float',
-    skill: 'baking pretzels',
-    imgUrl: '/static/img/float.jpg'
-  };
-
-  const [cards, updateCards] = React.useState([floatCard]);
+  React.useEffect(() => {
+    fetch("/cards.json")
+      .then((response) => response.json())
+      .then((data) => updateCards(data.cards));
+  }, []);
 
   const tradingCards = [];
+
+  // you can uncomment this console.log (line 27) to see the
+  // value of the cards object what is it initially?
+  // what about after the component re-renders?
+  // if you remove the empty array on line 18 (so
+  // there is no dependency list, what happens?)
+  // console.log({ cards });
 
   for (const currentCard of cards) {
     tradingCards.push(
@@ -70,13 +45,7 @@ function TradingCardContainer() {
     );
   }
 
-  return (
-    <div>{tradingCards}</div>
-  );
-
+  return <div>{tradingCards}</div>;
 }
 
-ReactDOM.render(
-  <TradingCardContainer />,
-  document.getElementById('container')
-);
+ReactDOM.render(<TradingCardContainer />, document.getElementById("container"));
